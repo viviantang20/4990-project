1eb9920c8327d077d091769aa770c5290fcfe34a
Nouman <noumancodes@gmail.com>
2023-10-02 15:35:51 -0400

    Initial theme

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" data-theme="dracula">
             <body className={inter.className}>{children}</body>
         </html>
     )
