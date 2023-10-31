7c2ce1c964467841b9a477e4c213dc1a89ea5746
Jack <hornsey15@hotmail.com>
2023-10-31 14:28:48 -0400

    sidebar design changes

--- components/sidebar.js
+++ components/sidebar.js
@@ -7,8 +7,8 @@ export default function Sidebar() {
     const pathname = usePathname();
 
     return (
-        <div className="menu menu-sm lg:menu-md px-2 py-0 border-l-8 border-white">
-            <li className="menu-title">Artificial Intelligence: A Modern Approach</li>
+        <div className="menu menu-sm lg:menu-md px-2 py-0 border-l-8 border-white h-screen top-0 sticky">
+            <li className="menu-title text-emerald-400 "> ❤ Artificial Intelligence: A Modern Approach </li>
             {Links.map((link) => (
                 <li key={link.id}>
                         <Link href={`${link.url}`} className={link.url == pathname ? "active" : ""}>{link.title}</Link>
