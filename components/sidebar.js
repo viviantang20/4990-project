b0071277e5ac5ef15827d52efdbcc14bc6f8c823
Nouman <noumancodes@gmail.com>
2023-10-30 21:04:08 -0400

    intro bug fixed

--- components/sidebar.js
+++ components/sidebar.js
@@ -5,7 +5,7 @@ export default function Sidebar() {
     return (
         <div className="menu menu-sm lg:menu-md px-2 py-0   border-l-8 border-white  ">
                 <li className="menu-title">Artificial Intelligence: A Modern Approach</li>
-                <li ><Link href="/lesson/intro">Introduction</Link></li>
+                <li ><Link href="/lesson/Introduction">Introduction</Link></li>
                 <li><Link href="/lesson/Intelligent-Agents">Intelligent Agents</Link></li>
                 <li><Link href="/lesson/Problem-Solving">Problem-Solving</Link></li>
                 <li><Link href="/lesson/Search-in-Complex-Environments">Search in Complex Environments</Link></li>
