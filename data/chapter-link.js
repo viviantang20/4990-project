92f87cc10d6008c14489b3f6402f7868ffe75cf6
Jaxon Hornsey <63931307+JaxonHornsey@users.noreply.github.com>
2024-01-23 00:27:55 -0500

    chapter numbers added

--- data/chapter-link.js
+++ data/chapter-link.js
@@ -1,47 +1,47 @@
 let chapterLinks = [
     {
         url: "/lesson/Introduction",
-        title: "Introduction",
+        title: "1. Introduction",
         id: 1,
     },
     {
         url: "/lesson/Intelligent-Agents",
-        title: "Intelligent Agents",
+        title: "2. Intelligent Agents",
         id: 2,
     },
     {
         url: "/lesson/Problem-Solving",
-        title: "Problem-Solving",
+        title: "3. Problem-Solving",
         id: 3,
     },
     {
         url: "/lesson/Search-in-Complex-Environments",
-        title: "Search in Complex Environments",
+        title: "4. Search in Complex Environments",
         id: 4,
     },
     {
         url: "/lesson/Constraint-Satisfaction-Problems",
-        title: "Constraint Satisfaction Problems",
+        title: "5. Constraint Satisfaction Problems",
         id: 5,
     },
     {
         url: "/lesson/Adversarial-Search-and-Games",
-        title: "Adversarial Search and Games",
+        title: "6. Adversarial Search and Games",
         id: 6,
     },
     {
         url: "/lesson/Logical-Agents",
-        title: "Logical Agents",
+        title: "7. Logical Agents",
         id: 7,
     },
     {
         url: "/lesson/First-Order-Logic",
-        title: "First-Order Logic",
+        title: "8. First-Order Logic",
         id: 8,
     },
     {
         url: "/lesson/Inference-in-First-Order-Logic",
-        title: "Inference in First-Order Logic",
+        title: "9. Inference in First-Order Logic",
         id: 9,
     },
 ];
