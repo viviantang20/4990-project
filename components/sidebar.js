c1d84281c5d9db25f67e2e97921c356925fed20e
Jack <hornsey15@hotmail.com>
2024-02-06 04:24:49 -0500

    added ch 4/8/9 to sidebar + info

--- components/sidebar.js
+++ components/sidebar.js
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import chapterLinks from "@/data/chapter-link";
 import algorithmLinks from "@/data/algorithm-link";
+import algorithmLinksCh4 from "@/data/algorithmLinksCh4";
+import algorithmLinksCh8 from "@/data/algorithmLinksCh8";
+import algorithmLinksCh9 from "@/data/algorithmLinksCh9";
 
 export default function Sidebar() {
     const pathname = usePathname();
@@ -13,7 +16,7 @@ export default function Sidebar() {
                 <h2 className="menu-title text-emerald-400 ">
                     ❤ Artificial Intelligence: A Modern Approach
                 </h2>
-                <details open>
+                <details open >
                     <summary>Textbook Chapters</summary>
                     <ul>
                         {chapterLinks.map((link) => (
@@ -30,10 +33,62 @@ export default function Sidebar() {
                         ))}
                     </ul>
                 </details>
-                <details open>
-                    <summary> Search Algorithms |  Chapter 3</summary>
+                <br/>
+                <details >
+                    <summary> Ch. 3 | Search Algorithms  <tab/>  </summary>
                     <ul>
                         {algorithmLinks.map((link) => (
+                            <li key={link.id}>
+                                <Link
+                                    href={`${link.url}`}
+                                    className={
+                                        link.url == pathname ? "active" : ""
+                                    }
+                                >
+                                    {link.title}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </details>
+                <details >
+                    <summary> Ch. 4  | Search Algorithms <tab/> </summary>
+                    <ul>
+                        {algorithmLinksCh4.map((link) => (
+                            <li key={link.id}>
+                                <Link
+                                    href={`${link.url}`}
+                                    className={
+                                        link.url == pathname ? "active" : ""
+                                    }
+                                >
+                                    {link.title}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </details>
+                <details >
+                    <summary>Ch. 8  | First-Order Logic Algorithms </summary>
+                    <ul>
+                        {algorithmLinksCh8.map((link) => (
+                            <li key={link.id}>
+                                <Link
+                                    href={`${link.url}`}
+                                    className={
+                                        link.url == pathname ? "active" : ""
+                                    }
+                                >
+                                    {link.title}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </details>
+                <details >
+                    <summary>Ch. 9  | Inference Rule Algorithms </summary>
+                    <ul>
+                        {algorithmLinksCh9.map((link) => (
                             <li key={link.id}>
                                 <Link
                                     href={`${link.url}`}
---
+++ data/algorithmLinksCh4.js
@@ -0,0 +1,24 @@
+let algorithmLinksCh4 = [
+    {
+        url: "/lesson/hill-climbing-search",
+        title: "Hill-climbing Search",
+        id: 20,
+    },
+    {
+        url: "/lesson/Breadth-first-search",
+        title: "Simulated annealing",
+        id: 21,
+    },
+    {
+        url: "/lesson/Dijkstra-algorithm",
+        title: "Local beam search",
+        id: 22,
+    },
+    {
+        url: "/lesson/Depth-limited-search",
+        title: " Evolutionary algorithms",
+        id: 23,
+    },
+];
+
+export default algorithmLinksCh4;
