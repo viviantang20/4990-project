a3f5e0b81bf6a4618f640a5f5912969a8426ee0d
Nouman <noumancodes@gmail.com>
2024-02-20 15:29:37 -0500

    fixing jaxons sloppy code

--- app/page.js
+++ app/page.js
@@ -1,22 +1,24 @@
 "use client";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
     return (
         <main className="h-screen w-screen flex align-center justify-center">
             <div className="h-full lg:w-1/2 flex flex-col justify-center items-center">
-                <h1 className="text-7xl">I <span className="text-red-500">❤</span> AI</h1>
+                <h1 className="text-7xl">
+                    I <span className="text-red-500">❤</span> AI
+                </h1>
                 <h2 className="text-xl mt-2">
                     Project by Jaxon, Nouman, and Vivian
                 </h2>
                 <div className="divider"></div>
                 <h2 className="text-lg">
-                    The AI course she told you not to worry about 
+                    The AI course she told you not to worry about
                 </h2>
-                
 
-                <Link href="/lesson/introduction">
-                    <a className="btn mt-4">Begin Your Journey</a> {/* Using an anchor tag styled as a button */}
+                <Link href="/lesson/Introduction">
+                    <a className="btn mt-4">Begin Your Journey</a>{" "}
+                    {/* Using an anchor tag styled as a button */}
                 </Link>
                 {/* <dialog
                     id="my_modal_5"
