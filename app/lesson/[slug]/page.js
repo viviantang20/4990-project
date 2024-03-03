ae8cdb1e8050f2805f01d821572dc67db37afaff
Jack <hornsey15@hotmail.com>
2024-03-03 16:31:13 -0500

    -style

--- app/lesson/[slug]/page.js
+++ app/lesson/[slug]/page.js
@@ -8,76 +8,70 @@ import quizData from "@/data/quizData";
 import Giscus from "@giscus/react";
 
 async function getData(url, headers) {
-    try {
-        const response = await axios.get(url, { headers });
-        const fileInfo = response.data;
-        const content = Buffer.from(fileInfo.content, "base64").toString(
-            "utf-8"
-        );
+  try {
+    const response = await axios.get(url, { headers });
+    const fileInfo = response.data;
+    const content = Buffer.from(fileInfo.content, "base64").toString("utf-8");
 
-        return content;
-    } catch (error) {
-        console.error(error);
-    }
+    return content;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default async function Page({ params }) {
-    // Set your GitHub username, repository name, and the file path
-    const githubUsername = "NoumanAMalik";
-    const repositoryName = "i-love-ai-docs";
+  // Set your GitHub username, repository name, and the file path
+  const githubUsername = "NoumanAMalik";
+  const repositoryName = "i-love-ai-docs";
 
-    // Exit early until we are ready to handle algorithms
-    if (params.slug.includes("algorithm"))
-        return (
-            <div className="w-full flex flex-row">
-                <Sidebar />
-            </div>
-        );
-
-    const filePath = params.slug + ".md";
-
-    // Replace 'your-token' with the personal access token you generated
-    const accessToken = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN;
-
-    // GitHub API endpoint to get a file
-    const apiUrl = `https://api.github.com/repos/${githubUsername}/${repositoryName}/contents/${filePath}`;
-
-    const headers = {
-        Authorization: `token ${accessToken}`,
-    };
-
-    let pageContent = await getData(apiUrl, headers);
-
+  // Exit early until we are ready to handle algorithms
+  if (params.slug.includes("algorithm"))
     return (
-        <div className="w-full flex flex-row">
-            <Sidebar />
-            <div className="flex-1  pl-8">
-                <div className="prose ">
-                    {pageContent ? (
-                        <Markdown>{pageContent}</Markdown>
-                    ) : (
-                        "Loading..."
-                    )}
-                </div>
-                <Quiz questions={quizData[params.slug] || []} />
-
-                <Giscus
-                    id="comments"
-                    repo="NoumanAMalik/i-love-ai-docs"
-                    repoId="R_kgDOKhC1Xw"
-                    category="Q&A"
-                    categoryId="DIC_kwDOKhC1X84Cc_Yl"
-                    mapping="pathname"
-                    term="Welcome to @giscus/react component!"
-                    reactionsEnabled="1"
-                    emitMetadata="0"
-                    inputPosition="top"
-                    theme="preferred_color_scheme"
-                    lang="en"
-                    loading="lazy"
-                    crossorigin="anonymous"
-                />
-            </div>
-        </div>
+      <div className="w-full flex flex-row">
+        <Sidebar />
+      </div>
     );
+
+  const filePath = params.slug + ".md";
+
+  // Replace 'your-token' with the personal access token you generated
+  const accessToken = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN;
+
+  // GitHub API endpoint to get a file
+  const apiUrl = `https://api.github.com/repos/${githubUsername}/${repositoryName}/contents/${filePath}`;
+
+  const headers = {
+    Authorization: `token ${accessToken}`,
+  };
+
+  let pageContent = await getData(apiUrl, headers);
+
+  return (
+    <div className="w-full flex flex-row ">
+      <Sidebar />
+      <div className="flex-1 flex flex-col justify-center items-center pl-8 ">
+        <div className="prose max-w-5xl">
+          {pageContent ? <Markdown>{pageContent}</Markdown> : "Loading..."}
+        </div>
+        <Quiz questions={quizData[params.slug] || []} />
+
+        <Giscus
+          id="comments"
+          repo="NoumanAMalik/i-love-ai-docs"
+          repoId="R_kgDOKhC1Xw"
+          category="Q&A"
+          categoryId="DIC_kwDOKhC1X84Cc_Yl"
+          mapping="pathname"
+          term="Welcome to @giscus/react component!"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme="preferred_color_scheme"
+          lang="en"
+          loading="lazy"
+          crossorigin="anonymous"
+        />
+      </div>
+    </div>
+  );
 }
