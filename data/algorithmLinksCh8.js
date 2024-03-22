72298bd143988f566118e0a1b856700d474017c0
Nouman <noumancodes@gmail.com>
2024-03-22 00:54:34 -0400

    fixed knowledge engineering

--- data/algorithmLinksCh8.js
+++ data/algorithmLinksCh8.js
@@ -1,19 +1,19 @@
 let chapter8AlgoLinks = [
-  {
-    url: "/lesson/hill-climbing-search",
-    title: "  Syntax and Semantics of First-Order Logic",
-    id: 80,
-  },
-  {
-    url: "/lesson/Breadth-first-search",
-    title: "Using First-Order Logic",
-    id: 81,
-  },
-  {
-    url: "/lesson/Dijkstra-algorithm",
-    title: "Knowledge Engineering ",
-    id: 82,
-  },
+    {
+        url: "/lesson/hill-climbing-search",
+        title: "  Syntax and Semantics of First-Order Logic",
+        id: 80,
+    },
+    {
+        url: "/lesson/Breadth-first-search",
+        title: "Using First-Order Logic",
+        id: 81,
+    },
+    {
+        url: "/lesson/Knowledge-Engineering",
+        title: "Knowledge Engineering ",
+        id: 82,
+    },
 ];
 
 export default chapter8AlgoLinks;
